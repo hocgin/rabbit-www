d1642ec3055caf583e1f5c7fccf0e46c121667a1
hocgin <hocgin@gmail.com>
2021-02-27 18:38:41 +0800

    update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,11 @@
 import styles from './index.less';
+import GinFooter from '@hocgin/gin-footer';
 
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
       {props.children}
+      <GinFooter/>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,71 @@
+import React from 'react';
 import styles from './index.less';
+import { connect } from 'dva';
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
+import { Link } from 'umi';
+import AppsModel from '@/models/apps';
+import { dispatchType } from '@/utils/model-utils';
+import classnames from 'classnames';
+
+@connect(({ global, apps, loading, ...rest }) => {
+  return {
+    paging: apps?.paging,
+    pagingLoading: loading.effects[dispatchType(AppsModel, AppsModel.effects.worked)],
+  };
+}, dispatch => ({
+  $worked: (args = {}) => dispatch({ type: dispatchType(AppsModel, AppsModel.effects.worked), ...args }),
+}))
+class index extends React.Component {
+
+  state = {
+    isOpenMenu: false,
+    selectedImg: null,
+  };
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+    // window.removeEventListener('resize', this.handleResize);
+  }
 
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  render() {
+    let {} = this.props;
+    let { isOpenMenu, selectedImg } = this.state;
+    return (<div className={styles.page}>
+      <section className={styles.section}>
+        <header>
+          <a href='#' className={styles.logo}>HOCGIN<span className={styles.suffix}>.top</span></a>
+          <div className={classnames(styles.toggle, {
+            [styles.active]: isOpenMenu,
+          })} onClick={this.onClickToggle}>{isOpenMenu ? <CloseOutlined /> : <MenuOutlined />}</div>
+          <ul className={classnames(styles.navigation, {
+            [styles.active]: isOpenMenu,
+          })}>
+            <li><Link to={'#'}>首页</Link></li>
+          </ul>
+        </header>
+        <div className={styles.container}>
+          {[1, 2, 3, 4].map((_, index) => (<div className={styles.box}
+                                                style={{ '--i': index }}
+                                                onClick={this.onClickSelected.bind(this, index)}>
+            <div className={classnames(styles.imgBx, { [styles.active]: selectedImg === index })}>
+              <img src={`http://cdn.hocgin.top/uPic/img_${index}.jpg`} alt='' />
+            </div>
+          </div>))}
+        </div>
+        <div>
+          <h2>🚀 我们都是自己的英雄</h2>
+        </div>
+      </section>
+    </div>);
+  }
+
+  onClickSelected = (index) => this.setState(({ selectedImg }) => ({
+    selectedImg: selectedImg === index ? null : index,
+  }));
+  onClickToggle = () => this.setState(({ isOpenMenu }) => ({ isOpenMenu: !isOpenMenu }));
 }
+
+export default index;
