0c3636043608cc7dfa5b0a12988c2ca9c4308787
hocgin <hocgin@gmail.com>
2021-07-16 22:47:59 +0800

    feat: website

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,6 +6,16 @@ function BasicLayout(props) {
     <div className={styles.normal}>
       {props.children}
       <GinFooter/>
+      <div className={styles.copytitle}>
+        © 2013-2021{' '}
+        <a href="http://www.hocgin.top/" target="_blank">
+          兔游工作室
+        </a>{' '}
+        |{' '}
+        <a href="https://beian.miit.gov.cn/" target="_blank">
+          闽ICP备20004537号
+        </a>
+      </div>
     </div>
   );
 }
