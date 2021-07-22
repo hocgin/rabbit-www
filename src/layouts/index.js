c90a109528d5062e7940499c5f570f02327aade0
hocgin <hocgin@gmail.com>
2021-07-22 22:32:06 +0800

    feat: website

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,21 +1,11 @@
 import styles from './index.less';
-import GinFooter from '@hocgin/gin-footer';
+import { Footer } from '@hocgin/ui';
 
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
       {props.children}
-      <GinFooter/>
-      <div className={styles.copytitle}>
-        © 2013-2021{' '}
-        <a href="http://www.hocgin.top/" target="_blank">
-          兔游工作室
-        </a>{' '}
-        |{' '}
-        <a href="https://beian.miit.gov.cn/" target="_blank">
-          闽ICP备20004537号
-        </a>
-      </div>
+      <Footer />
     </div>
   );
 }
